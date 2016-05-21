d447b90e1d38b9f640a9c3cf74516890b0b75af7
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-05-21 16:20:37 +1000

    set up statistics activity

--- app/src/main/java/com/example/jake_/cp3307_a1/StatisticsActivity.java
+++ app/src/main/java/com/example/jake_/cp3307_a1/StatisticsActivity.java
@@ -2,13 +2,22 @@ package com.example.jake_.cp3307_a1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        listView  = (ListView) findViewById(R.id.listView);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
 
